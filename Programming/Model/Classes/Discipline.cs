090b61224df93a6a13ee59a706dfe7131813bba9
Гаврилов Саша <ugadaihto@gmail.com>
2023-03-03 20:07:53 +0700

    Созданы классы, добавлены поля классов

---
+++ Programming/Model/Classes/Discipline.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Model.Classes
+{
+    internal class Discipline
+    {
+        private string name;
+        private string teacher;
+        private int mark;
+    }
+}
---
+++ Programming/Model/Classes/Flight.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Model.Classes
+{
+    internal class Flight
+    {
+        private string departure;
+        private string destination;
+        private int time;
+    }
+}
